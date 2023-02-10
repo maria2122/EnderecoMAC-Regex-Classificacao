6515ceb14c95b359fd972ca4d293cac190d56dfc
Isaias Souza <isaias.souza.spfc@gmail.com>
2023-02-09 23:27:20 -0300

    teste git extensions

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import re
+import re # sfsfs
 # [A-F0-9][A-F0-9]:[A-F0-9][A-F0-9]:[A-F0-9][A-F0-9]:[A-F0-9][A-F0-9]:[A-F0-9][A-F0-9]:[A-F0-9][A-F0-9]:[A-F0-9][A-F0-9]:[A-F0-9][A-F0-9]
 
 # text = '55:9F:E4:69:87:C5 55:95:E4:69:87:C5 batata'
@@ -6,7 +6,7 @@ import re
 
 results = re.compile(r'((([\w+]{2}[:]){5})[\w+]{2})')
 total = results.findall('fsfsf55:9F:E4:69:87:C5batata')
-print(total)
+print(total) 
 # groups_match = results.groups()
 # if groups_match is None:
 #     exit()
