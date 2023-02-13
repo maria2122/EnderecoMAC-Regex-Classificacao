140583bcc8a26fb172862cff566337192a463517
Maria Jaqueline Dos Santos Silva <58671604+maria2122@users.noreply.github.com>
2023-02-13 20:08:52 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,12 @@
 from regex import search
 from classifier import classify
-text = 'E0-06-E6-CE-D9-10\n\
-        fsfsf00-D7-6D-70-E4-E6batata\n\
-        E8-11-32-B8-CF-CBbatata\n\
-        fsfsf55-94-E2-3C-71-A8-B3\n'
+text = 'Fui no E0-06-E6-CE-D9-10mercadinho\nPra fazer00-D7-6D-70-E4-E6isso'\
+       'mesmoE8-11-32-B8-CF-CBe chegando la.A pipa foi cortada94-E2-3C-71-A8-B3'
 result = search(text)
-print(classify(result))
+for item in classify(result):
+   print(item)
 
 # E0-06-E6-CE-D9-10
 # 00-D7-6D-70-E4-E6
 # E8-11-32-B8-CF-CB
-# 55-94-E2-3C-71-A8
+# 94-E2-3C-71-A8-B3
